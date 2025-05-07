eaf4797d91d84e314152dd4dd000406476c4d0c9
Thanat <stampstrk@gmail.com>
2025-05-07 22:19:56 +0700

    add graphql2

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,14 @@
-module.exports = () => ({});
+export default () => ({
+    graphql: {
+      config: {
+        endpoint: '/graphql',
+        shadowCRUD: true,
+        landingPage: false, // disable Sandbox everywhere
+        depthLimit: 7,
+        amountLimit: 100,
+        apolloServer: {
+          tracing: false,
+        },
+      },
+    },
+  })
