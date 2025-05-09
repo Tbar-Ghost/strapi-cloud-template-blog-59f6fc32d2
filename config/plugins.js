5b5597a372507c7bd5b6da340e59b3414283e41e
Thanat <stampstrk@gmail.com>
2025-05-09 15:04:48 +0700

    add plugingraphql

--- config/plugins.js
+++ config/plugins.js
@@ -1,9 +1,13 @@
-module.exports = {
+module.exports = () => ({
     graphql: {
       enabled: true,
       config: {
+        endpoint: '/graphql',
+        shadowCRUD: true,
         playgroundAlways: true,
+        depthLimit: 7,
+        amountLimit: 100,
       },
     },
-  };
-
+  });
+  
